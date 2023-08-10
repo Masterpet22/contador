0674f24343a0b3ebc0500d499307222d5ff13b24
Masterpet <eprenza@gmail.com>
2023-08-10 10:10:37 -0400

    se crearon las funciones que manejan la interactividad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Boton from './componentes/boton';
 import logo from './img/freecodecamp-logo.png'
 
 function App() {
+  const manejar_Clic = () => {
+    console.log("clic")
+  }
+  const reiniciarContador = ()=>{
+    console.log("reiniciar")
+  }
+
+
   return (
     <div className="App">
       <div className='logo-contenedor'>
@@ -11,7 +20,14 @@ function App() {
           alt='logo'/>
       </div>
       <div className='contenedor'>
-        
+        <Boton
+        texto='Clic'
+        esCLick={true}
+        manejarClic={manejar_Clic}/>
+        <Boton 
+        texto={'Reiniciar'}
+        esCLick={false}
+        manejarClic={reiniciarContador}/>
       </div>
     </div>
   );
